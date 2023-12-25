d6195a15e7c945cb0b98f05c8c7898efddcb442d
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-12-25 22:58:47 +0530

    feat: faker package install

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { faker } from "@faker-js/faker"
 
 const App = () => {
+  console.log(faker.commerce.product(), "fake data")
   return <div className=" w-full min-h-screen bg-red-300">Hello Zevi Shop</div>;
 };
 
