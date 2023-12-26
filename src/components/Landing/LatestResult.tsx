da2ef5cdb988ce9c53e1bd53db518760d8e8be52
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-12-26 12:13:28 +0530

    feat: lastest result section

--- src/components/Landing/LatestResult.tsx
+++ src/components/Landing/LatestResult.tsx
@@ -7,12 +7,30 @@ const LatestResult = () => {
   const popularSuggestion: string[] = getPopularSuggestion(5);
 
   return (
-    <div
-      className=" w-full h-10 bg-white rounded mt-2 shadow-md"
-    >
-      <Link to="/products">
-        <h1>Latest Trends</h1>
-      </Link>
+    <div className=" w-full bg-white rounded mt-2 shadow-md p-4 flex flex-col gap-4 max-h-80 overflow-y-scroll">
+      <h1 className=" font-semibold text-base">Latest Trends</h1>
+      <div className=" grid md:grid-cols-5 md:grid-rows-1 grid-cols-3 grid-rows-2 gap-4">
+        {latestTrends.map((item) => {
+          return (
+            <Link to="/products">
+              <div className=" flex flex-col items-start">
+                <img src={item.productImg} className="object-contain" />
+                <h1 className=" text-xs max-w-[20ch]">{item.productName}</h1>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <h1 className="font-semibold text-base">Popular suggestion</h1>
+      <div className=" flex flex-col items-start gap-2">
+        {popularSuggestion.map((item) => {
+          return (
+            <Link to="/products">
+              <h1 className=" text-xs ">{item}</h1>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
