20a4a7143af00c6f4a27363a2ab77d8e307876b0
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-12-26 19:39:20 +0530

    minor: text size

--- src/screens/products/index.tsx
+++ src/screens/products/index.tsx
@@ -9,7 +9,7 @@ const ProductsScreen = () => {
       <div className=" flex-1 px-6 flex flex-col">
         <div className=" flex-1 flex mt-8 ">
           <div className=" md:w-1/4 w-1/3 ">
-            <h1 className=" md:text-2xl sm:text-sm text-xs font-medium mb-4">
+            <h1 className=" md:text-2xl sm:text-sm  font-medium mb-4">
               Search Results
             </h1>
             <FilterSection />
