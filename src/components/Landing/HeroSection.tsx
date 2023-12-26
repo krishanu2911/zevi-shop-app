30c6784a821b92212b475ec86d8a177f0a71a854
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-12-26 12:43:21 +0530

    feat: search bar rendering optimization

--- src/components/Landing/HeroSection.tsx
+++ src/components/Landing/HeroSection.tsx
@@ -1,28 +1,19 @@
-import { useRef, useState, FocusEvent } from "react";
-import SearchBar from "../SearchBar";
+import { useRef, useState } from "react";
 import LatestResult from "./LatestResult";
+import HeroSearchBar from "./HeroSearchBar";
 
 const HeroSection = () => {
   const [searchBarFocused, setSearchBarFocused] = useState<boolean>(false);
   const containerRef = useRef<HTMLDivElement>(null);
-  const onFocusHandler = () => {
-    setSearchBarFocused(true);
-  };
-
-  const onBlurHandler = (event: FocusEvent) => {
-    if (
-      containerRef.current &&
-      !containerRef.current.contains(event.relatedTarget)
-    ) {
-      setSearchBarFocused(false);
-    }
-  };
 
   return (
     <div ref={containerRef} className="w-full h-full flex justify-center pt-16">
       <div className=" md:w-1/2 w-3/4 h-12 relative">
         <div className=" w-full px-6">
-          <SearchBar onFocusFunc={onFocusHandler} onBlurFunc={onBlurHandler} />
+          <HeroSearchBar
+            setSearchBarFocused={setSearchBarFocused}
+            containerRef={containerRef}
+          />
         </div>
         {searchBarFocused ? (
           <div className=" w-full absolute">
